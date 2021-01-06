79a26588d996d343cc3bf22e42d28313e7cb844d
rabidsimon <76966797+rabidsimon@users.noreply.github.com>
2021-01-06 12:20:49 +0100

    Commented Code

--- MinesweeperFX/src/Main.java
+++ MinesweeperFX/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		Board game = new Board(20, 10);
-		game.populateBoard(20);
-		System.out.print(game);
+		Board game = new Board(20, 10);																//Constructs a 20 by 10 board
+		game.populateBoard(20);																		//Populates board with 20 bombs
+		System.out.print(game);																		//Prints board to console
 	}
 }
--- MinesweeperFX/src/Tile.java
+++ MinesweeperFX/src/Tile.java
@@ -2,33 +2,33 @@ public class Tile {
 	private Boolean isBomb, isVisible;
 	private int neighbours;
 	
-	public Tile() {
+	public Tile() {																					//Constructs empty tile
 		this.isBomb = false;
 		this.isVisible = false;
 		this.neighbours = 0;
 	}
 	
-	public void setBomb() {
+	public void setBomb() {																			//Places bomb on tile
 		this.isBomb = true;
 	}
 	
-	public void setNeighbours(int count) {
+	public void setNeighbours(int count) {															//Updates neighbour counter
 		this.neighbours = count;
 	}
 	
-	public void revealTile() {
+	public void revealTile() {																		//Reveals tile
 		this.isVisible = true;
 	}
 	
-	public Boolean isTileBomb() {
+	public Boolean isTileBomb() {																	//Returns boolean bomb value of tile
 		return this.isBomb;
 	}
 	
-	public Boolean isTileVisible() {
+	public Boolean isTileVisible() {																//Returns boolean visibility value of tile
 		return this.isVisible;
 	}
 	
-	public int getNeighbours() {
+	public int getNeighbours() {																	//Return int value of number of adjacent bombs
 		return this.neighbours;
 	}
 }
